117aab8e5faa5ae8c8664827afaaae73648bb409
Gonzalo <goznalomail@gmail.com>
2021-05-03 12:06:58 +0200

    Things to check out, initial setup

--- waveform-internet.py
+++ waveform-internet.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+'''
+https://web.archive.org/web/20161203074728/http://jaganadhg.freeflux.net:80/blog/archive/2009/09/09/plotting-wave-form-and-spectrogram-the-pure-python-way.html
+'''
 import sys
 from pylab import *
 import wave
@@ -6,7 +10,7 @@ import wave
 def show_wave_n_spec(speech):
     spf = wave.open(speech,'r')
     sound_info = spf.readframes(-1)
-    sound_info = fromstring(sound_info, int)
+    sound_info = frombuffer(sound_info, int)
 
     f = spf.getframerate()
    
